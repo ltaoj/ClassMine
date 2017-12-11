788627c932b204ff6c4c41454f5a6badaa0d2382
taojiangli <taoj0417@outlook.com>
2017-12-11 23:09:34 +0800

    fix mudule import problem

--- csujwc/tests/auth_test.py
+++ csujwc/tests/auth_test.py
@@ -1,4 +1,7 @@
-import common
+import sys, os
+configpath = os.path.dirname(os.path.realpath(__file__)).replace(os.path.sep + 'tests', '')
+sys.path.append(configpath)
+
 from login import Context
 from login import CodeLogin
 
