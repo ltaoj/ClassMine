1bbc95d1a3a5677013e8f6e88b4b645a0ecefaa0
taojiangli <taoj0417@outlook.com>
2017-12-14 01:10:16 +0800

    add constants and config

--- csujwc/contants/CONFIG.py
+++ csujwc/contants/CONFIG.py
@@ -1,5 +1,6 @@
 
 PHANTOMJS_SOURCES = ['https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2']
+#PHANTOMJS_PATH = './phantomjs-2.1.1-linux-x86_64/bin/phantomjs'
 PHANTOMJS_PATH = ''
 PHANTOMJS_NAME = 'phantomjs'
 SCREENSHOT_NAME = "screenshot.png"
@@ -9,3 +10,5 @@ CODE_IMAGE_NAME = "code.png"
 CODE_RECOGNIZED_FILE_NAME = "code"
 USE_OCR = True
 SUBMIT_BTN_ID = "btnSubmit"
+# relevate to login.py
+ENCODED_HTML_PATH = "./template/index.html"
--- csujwc/contants/URL.py
+++ csujwc/contants/URL.py
@@ -1,2 +1,2 @@
 LOGIN_PAGE_WITHCODE = "http://csujwc.its.csu.edu.cn/"
-LOGIN_API_NORMAL = ""
+LOGIN_API_NORMAL = "http://csujwc.its.csu.edu.cn/jsxsd/xk/LoginToXk"
